59c0f596399bf98f51da465ae5ecf86e2e2d3a44
sridharreddapani <sridhar@intersect.org.au>
2013-06-05 09:07:46 +1000

    Modified inc.pp with class{} instead of include

--- manifests/classes/inc.pp
+++ manifests/classes/inc.pp
@@ -1,4 +1,8 @@
-include easypuppet
-include user
-include apache
-include ssh
+#include easypuppet
+#include user
+#include apache
+#include ssh
+class{"easypuppet":}
+class{"user":}
+class{"apache":}
+class{"ssh":}
