f1267088b80785e6fb872759cc0d5343f0da82ee
sridharreddapani <sridhar@intersect.org.au>
2013-06-04 14:28:45 +1000

    updated users.pp added group admin

--- classes/users.pp
+++ classes/users.pp
@@ -1,10 +1,18 @@
 class user {
+	group {'admin':
+		ensure => present,
+		gid => 1000,
+	}	
+			
 	user {'sridhar':
 		ensure => present,
 		uid    => 501,
+		gid    => 'admin',
 	}
 	user {'user01':
 		ensure => present,
                 uid    => 502,
+		gid    => 'admin',
         }
 }
+
