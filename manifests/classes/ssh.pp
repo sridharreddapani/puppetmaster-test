1c4de3f15a2577afc4b80d29ce57d20997368f8e
sridharreddapani <sridhar@intersect.org.au>
2013-06-04 15:35:48 +1000

    added master key to ssh

--- manifests/classes/ssh.pp
+++ manifests/classes/ssh.pp
@@ -1,6 +1,9 @@
 class ssh {
-	package {'sshd':
+	package {'openssh':
 		ensure => installed,
+	}
+	package {'openssh-clients':
+	        ensure => installed,
 	}
 	file {'/etc/ssh/sshd_config':
 		ensure => file,
@@ -9,6 +12,20 @@ class ssh {
 		mode   => '0600',
 		notify => Service['sshd'],
 	}
+	file {'/root/.ssh/authorized_keys':
+		ensure => file,
+		owner  => 'root',
+		group  => 'root',
+		mode   => '0640',
+		source => "puppet:///files/master_id_rsa.pub",
+	}
+	file {'/root/.ssh':
+	        ensure => directory,
+		owner  => 'root',
+		group  => 'root',			
+		mode   => '0700',
+	}
+									        }
 	service {'sshd':
 		ensure => running,
 	}
