e615d812bfa8c3860030a98415db95f48d783beb
sridharreddapani <sridhar@intersect.org.au>
2013-06-05 09:21:36 +1000

    Modified inc.pp with class{} instead of include

--- manifests/classes/tree.pp
+++ manifests/classes/tree.pp
@@ -1,7 +1,9 @@
 package {'tree':
 	ensure => installed,
 }
-#notify {"Hello Agent tree is completed": }
+notify {"Hello Agent tree is completed": 
+	message => "apche config file from apche class is ${apche::apache_conf}"
+}
 #notify {'bignote':
 #	message => 'Hello this is going to test notifu with meassage',
 #}
