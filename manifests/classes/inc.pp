2644815149627307ed550ba647d5138cc65953cc
sridharreddapani <root@puppetmaster.sridhar.com>
2013-06-05 09:15:27 +1000

    modified inc.pp to test variable scopping

--- manifests/classes/inc.pp
+++ manifests/classes/inc.pp
@@ -6,6 +6,7 @@ class{"easypuppet":}
 class{"user":}
 class{"apache":}
 class{"ssh":}
+# Testing Variable Scopping
 notify {'varcheck':
 #	message => "Apache Config From Apache Class: ${::apache::apache_conf}",
 	message => "Apache Config From Apache Class: ${apache::apache_conf}",
