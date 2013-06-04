c33bf00dd732f968c18df526e7593db80ca22491
sridharreddapani <sridhar@intersect.org.au>
2013-06-04 15:04:07 +1000

    updated tree.pp to test notify with message

--- manifests/classes/tree.pp
+++ manifests/classes/tree.pp
@@ -1,4 +1,7 @@
 package {'tree':
 	ensure => installed,
 }
-#notify {"Hello Agent tree is completed": }
+notify {"Hello Agent tree is completed": }
+notify {'bignote':
+	message => 'Hello this is going to test notifu with meassage',
+}
